1db70b7a7f2cd39944dcc115ece025063f081c94
Fabbediklabbe <blabbe.ny@gmail.com>
2025-04-08 11:13:05 +0200

    Integrerat Springboot

---
+++ src/main/java/com/grupp16/Tornedalen/Konsthall/TornedalenKonsthallApplication.java
@@ -0,0 +1,13 @@
+package com.grupp16.Tornedalen.Konsthall;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TornedalenKonsthallApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TornedalenKonsthallApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/grupp16/Tornedalen/Konsthall/TornedalenKonsthallApplicationTests.java
@@ -0,0 +1,13 @@
+package com.grupp16.Tornedalen.Konsthall;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TornedalenKonsthallApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
